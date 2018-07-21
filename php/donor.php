7fd31a4a3e5829c02f54a054728be14ddd21f77c
prerananaik <prerananaik58@gmail.com>
2018-07-22 02:41:30 +0530

    updated donor html

--- php/donor.php
+++ php/donor.php
@@ -16,7 +16,7 @@ if(isset($_POST['btn_login']))
         //Admin Login
           $_SESSION['username'] = $row['username'];
             $_SESSION['name'] = $row['name'];
-            header("Location: makeapayment.php");
+            header("Location: index.php");
  
     }
     
@@ -41,7 +41,7 @@ if(isset($_POST['btn_register']))
         $_SESSION['name'] = $row['name'];
 		?>
         <script>alert("Successfully Registered");
-            window.location.href='makeapayment.php';</script>
+            window.location.href='index.php';</script>
         <?php
        
 	}
