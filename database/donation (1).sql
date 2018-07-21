edd6da665ab86ba36e9d8012e5ba16463679b723
Aishu1996 <aishub02@gmail.com>
2018-07-22 01:14:26 +0530

    updated donation.sql

---
+++ database/donation (1).sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 21, 2018 at 09:42 PM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 5.6.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vcare`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donation`
+--
+
+CREATE TABLE `donation` (
+  `username` varchar(40) NOT NULL,
+  `amount` int(255) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `donation`
+--
+
+INSERT INTO `donation` (`username`, `amount`, `date`) VALUES
+('Bill Gates', 3600000, '2018-07-17'),
+('Mark Zuckerberg', 45000000, '2018-07-18'),
+('Dimon Simon', 6799000, '2018-07-17'),
+('Richard Grey', 67000000, '2018-07-24'),
+('Tini Gates', 890000, '2018-07-31');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
