6cd72b5357b7f2b706e43dc1911eca0dd20f5b93
sujay2015 <2015sujay.varma@ves.ac.in>
2018-07-22 01:39:04 +0530

    Added logout form for each page

--- php/addpatient.php
+++ php/addpatient.php
@@ -2,6 +2,7 @@
 session_start();
 if(isset($_POST['btn_login']))
 {
+    $volunteer=$_SESSION['username'];
     $name=$_POST['name'];
     $fileno=$_POST['fileno'];
     $hospital=$_POST['hospital'];
@@ -9,8 +10,8 @@ if(isset($_POST['btn_login']))
     $type=$_POST['type'];
     $support=$_POST['support'];
     $date=date();
-    $query=mysqli_query($link,"INSERT INTO patient VALUES ('','$name','$fileno','$hospital','$gender','$type','$date')");
-    $id = mysqli_query($link,"SELECT id FROM patient where name='$name'");
+    $query=mysqli_query($link,"INSERT INTO patient VALUES ('','$name','$fileno','$hospital','$gender','$type','$date','$volunteer')");
+    $id = mysqli_query($link,"SELECT id FROM patient WHERE name='$name'");
     $query=mysqli_query($link,"INSERT INTO programme VALUES ('','$id','$type')");
     
     echo "<h1>New Patient Added</h1>"
