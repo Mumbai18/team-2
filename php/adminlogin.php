40e256692cabef3c2ee798c7a1262efd53dfb8d7
prerananaik <prerananaik58@gmail.com>
2018-07-22 09:35:41 +0530

    Admin changes

--- php/adminlogin.php
+++ php/adminlogin.php
@@ -6,8 +6,7 @@ mysqli_select_db($link,"vcare") or die('DATABSE NOT SELECTED');
 if(isset($_POST['btn_login']))
 {
     
-    $user_name = "admin123";
-    $password = "admin123";
+   
      header("Location: ../ui/charts.html");
 
    
